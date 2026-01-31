8c73a86e3a87cbdc10a0af2385eb5a2855d48314
Rob Ribeiro <robsonsky@gmail.com>
2026-01-31 13:07:03 -0300

    feat(adapters-android): android library module skeleton

---
+++ adapters-android/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library") version "8.2.2"
+    kotlin("android") version "1.9.24"
+}
+
+android {
+    namespace = "com.hailie.adapters"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":ports"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    testImplementation(kotlin("test"))
+}
